4834842ec90d5804e84d6ef2905ca7dee95de11a
karel <karel@dyn16.vrl.jyu.fi>
2022-06-07 14:50:13 +0300

    Made client a class

--- client.py
+++ client.py
@@ -2,44 +2,55 @@ import socket
 import threading
 from time import sleep
 
-HEADER = 128
-PORT = 5050
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "172.16.160.25"
-ADDR = (SERVER, PORT)
+class client:
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-connected = True
-print("[Connected]")
+    def __init__(self):
+        self.HEADER = 128
+        self.PORT = 5050
+        self.FORMAT = "utf-8"
+        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.SERVER = "172.16.160.16"
+        self.ADDR = (self.SERVER, self.PORT)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b" " * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.ADDR)
+        self.connected = True
+        print("[Connected]")
 
-def listen():
-    while connected:
-        msg_length = client.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = client.recv(msg_length).decode(FORMAT)
-            print(f"{msg}")
+    def send(self, msg):
+        message = msg.encode(self.FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(self.FORMAT)
+        send_length += b" " * (self.HEADER - len(send_length))
+        self.client.send(send_length)
+        self.client.send(message)
 
-thread = threading.Thread(target=listen)
-thread.start()
+    def listen(self):
+        while self.connected:
+            msg_length = self.client.recv(self.HEADER).decode(self.FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = self.client.recv(msg_length).decode(self.FORMAT)
+                print(f"{msg}")
 
-while connected:
-    message = input()
-    if message == "exit":
-        send(DISCONNECT_MESSAGE)
-        connected = False
-        sleep(1)
-    else:
-        send(message)
+    
+    def start(self):
+        thread = threading.Thread(target=self.listen)
+        thread.start()
+        while self.connected:
+            message = input()
+            if message == "exit":
+                self.send(self.DISCONNECT_MESSAGE)
+                self.connected = False
+                sleep(1)
+            else:
+                self.send(message)
 
-print("[Disconnected]")
+        print("[Disconnected]")
+
+def main():
+    cli = client()
+    cli.start()
+    
+if __name__ == "__main__":
+    main()

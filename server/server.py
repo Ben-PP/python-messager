8cde04a73a3590410f9c023b1643082850d83da5
karel <karel@dyn16.vrl.jyu.fi>
2022-06-07 14:32:15 +0300

    Merged server.py and handle_client.py into server class

--- server/server.py
+++ server/server.py
@@ -1,10 +1,7 @@
 import socket
 import threading
 
-import handle_client
-
 class server:
-
 
     def __init__(self):
         self.PORT = 5050
@@ -19,14 +16,54 @@ class server:
         self.server.bind(self.ADDR)
         self.serverup = True
 
+        self.clients = []
+    
+    def add_client(self, client):
+        self.clients.append(client)
+
+    def respond(self, conn, msg):
+        for client in self.clients:
+            if msg == self.DISCONNECT_MESSAGE:
+                message = msg.encode(self.FORMAT)
+                msg_length = len(message)
+                send_length = str(msg_length).encode(self.FORMAT)
+                send_length += b" " * (self.HEADER - len(send_length))
+                client[0].send(send_length)
+                client[0].send(message)
+
+            elif client[0] != conn:
+                message = msg.encode(self.FORMAT)
+                msg_length = len(message)
+                send_length = str(msg_length).encode(self.FORMAT)
+                send_length += b" " * (self.HEADER - len(send_length))
+                client[0].send(send_length)
+                client[0].send(message)
+
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
+
+        connected = True
+        while connected:
+            msg_length = conn.recv(self.HEADER).decode(self.FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(self.FORMAT)
+                if msg == self.DISCONNECT_MESSAGE:
+                    connected = False
+                    self.clients.remove((conn, addr))
+                print(f"[{addr}] {msg}")
+                self.respond(conn, f"[{str(addr[0])}] {msg}")
+
+        conn.close()
+
     def start(self):
         self.server.listen()
         print(f"[LISTENING] server is listening on: {self.SERVER}:{self.PORT}")
 
         while self.serverup:
             conn, addr = self.server.accept()
-            handle_client.add_client((conn, addr))
-            thread = threading.Thread(target=handle_client.handle_client, args=(conn, addr))
+            self.add_client((conn, addr))
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
             thread.start()
             print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 

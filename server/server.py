bd57b5e9879648473df8a40f95bb6ce17b3ac243
karel <karel@dyn16.vrl.jyu.fi>
2022-06-07 14:23:11 +0300

    Made server a class

--- server/server.py
+++ server/server.py
@@ -3,31 +3,38 @@ import threading
 
 import handle_client
 
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
+class server:
 
-HEADER = 128
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
 
-threads = []
+    def __init__(self):
+        self.PORT = 5050
+        self.SERVER = socket.gethostbyname(socket.gethostname())
+        self.ADDR = (self.SERVER, self.PORT)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-serverup = True
-            
-def start():
-    server.listen()
-    print(f"[LISTENING] server is listening on: {SERVER}:{PORT}")
+        self.HEADER = 128
+        self.FORMAT = "utf-8"
+        self.DISCONNECT_MESSAGE = "!DISCONNECT"
 
-    while serverup:
-        conn, addr = server.accept()
-        handle_client.add_client((conn, addr))
-        thread = threading.Thread(target=handle_client.handle_client, args=(conn, addr))
-        thread.start()
-        threads.append(thread)
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.ADDR)
+        self.serverup = True
 
-print("[STARTING] server is starting...")
-start()
+    def start(self):
+        self.server.listen()
+        print(f"[LISTENING] server is listening on: {self.SERVER}:{self.PORT}")
+
+        while self.serverup:
+            conn, addr = self.server.accept()
+            handle_client.add_client((conn, addr))
+            thread = threading.Thread(target=handle_client.handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+    print("[STARTING] server is starting...")
+    
+def main():
+    ser = server()
+    ser.start()
+
+if __name__ == "__main__":
+    main()
